f5fca2ece36c30b5dbe2369abecf1be575e889c3
Ebenezer Eshetie <77492217+EbenGitHub@users.noreply.github.com>
2023-03-05 17:04:01 +0300

    part1 mod

--- part1/rate-repository-app/App.js
+++ part1/rate-repository-app/App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ddd',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
